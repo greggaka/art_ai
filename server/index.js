e4e006abe696fc540d3774bcea413985e0369a4d
greggaka <greggakamine@gmail.com>
2023-03-07 22:59:35 -1000

    updated cors origin in backend

--- server/index.js
+++ server/index.js
@@ -9,7 +9,9 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://dalle-artai-clone.onrender.com"
+}));
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);
