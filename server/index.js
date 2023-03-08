3430e69f5e92661a699ad4a17589162d890d3ad4
greggaka <greggakamine@gmail.com>
2023-03-08 10:26:53 -1000

    updated cors origin to domain name

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: 'https://dalle-artai-clone.onrender.com',
+  origin: 'artaiclone.tech',
   methods: 'GET, POST',
 }));
 app.use(express.json({ limit: '50mb' }));
