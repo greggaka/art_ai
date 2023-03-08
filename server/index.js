3f29ff1a5c57eb3adb3495b8b8fcb056eb640ff5
greggaka <greggakamine@gmail.com>
2023-03-08 10:23:53 -1000

    updated cors origin

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: 'https://dalle_clone.onrender.com',
+  origin: 'https://dalle-artai-clone.onrender.com',
   methods: 'GET, POST',
 }));
 app.use(express.json({ limit: '50mb' }));
